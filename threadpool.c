5bc13980e193e2f5809642e812d5aebb00de6ccb
kail-cali <kail.sttar09@gmail.com>
2023-06-12 17:01:15 +0900

    Add : Module for thread pool ============================================== - add job scheduler

--- threadpool.c
+++ threadpool.c
@@ -20,6 +20,19 @@
 struct Cluster;
 struct Worker;
 
+/*---------------------*/
+
+
+typedef struct Context {
+    int max_epoll_event;
+    char* root;
+    int num_worker;
+    int epoll_timeout;
+
+
+} Context;
+
+
 typedef struct Container{
     int worker_id;
     //char buufer[1024];
@@ -47,10 +60,24 @@ typedef struct JobQueue{
 
 } JobQueue;
 
+
+typedef struct JobScheduler{
+    int num_lock;
+    int len;
+    
+    pthread_mutex_t rxmutex;
+    int* bit;
+
+
+} JobScheduler;
+
+
 typedef struct Worker{
     int id;
     pthread_t pthread;
-    Job* job // container for new Job
+    Job* job;// container for new Job
+    struct Cluster* cluster;
+    
 
 } Worker; 
 
@@ -74,6 +101,9 @@ typedef struct Cluster{
 } Cluster;
 
 
+
+
+int SERVICE_KEEPALIVE;
 
 
 /*********************************************************************************/
@@ -184,11 +214,122 @@ static struct Job* pop_front(JobQueue* job_queue){
 }
 
 
+static void* worker_handler(Worker* worker){
+    char worker_name[16] = {0};
+    snprintf(worker_name, 16, "worker#%d", worker->id);
+    Cluster* cluster = worker -> cluster;
+    int self = worker -> id;
+    char stage[2];
+
+    while (SERVICE_KEEPALIVE){
+        int status = read(cluster -> worker_pipe[self][0], stage, sizeof(stage));
+
+        if ( status < 0){
+            continue;
+        }
+
+        void(* handler)(void* );
+        void* args;
+        Job* new_job = worker->job;
+        if(new_job ){
+            handler = new_job -> handler;
+            args = new_job -> args;
+
+            handler(args);
+
+        }
+
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+static int pipe_init(Cluster* cluster, int num_worker){
+    cluster -> worker_pipe = (int**)malloc(num_worker * sizeof(int* ));
+    cluster -> cluster_pipe = (int* )malloc(2 * sizeof(int));
+
+    pipe(cluster -> cluster_pipe);
+
+    cluster -> schedule_event_fd = epoll_create(1024);
+
+    cluster -> schedule_events = (struct epoll_event* )malloc(1024 * sizeof(struct epoll_event));
+
+
+
+
+
+    return 0;
+}
+
+static int worker_init(Cluster* cluster, struct Worker** worker, int id){
+    
+    *worker = (struct Worker* )malloc(sizeof(struct Worker ));
+    if (worker == NULL){
+        err("worker_init():: allocate worker thread failed \n");
+        return -1;
+    }
+
+    (*worker) - > cluster = cluster;
+    (*worker) -> id = id;
+    pthrad_create(&(*worekr)->pthread, NULL, (void* (*)(void* )) worker_handler, (*worker));
+    pthread_detach((*worker)->pthread);
+    
+    return 0;
+}
+
+
+struct Cluster* cluster_init_with_context(Context* context){
+    Cluster* cluster;
+
+
+    return cluster;
+}
+
+struct Cluster* cluster_init(int num_worker){
+
+    Cluster* cluster;
+    cluster = (struct Cluster* )malloc(sizeof(struct Cluster));
+    
+    if (cluster == NULL){
+
+        err("cluster_init():: allocate cluster on memory failed \n");
+        return NULL;
+    }
+
+    cluster -> num_worker = num_worker;
+    
+
+
+
+
+
+    // worker thread init
+    cluster -> workers = (struct Worker** )malloc(num_worker * sizeof(struct Worker* ));
+    if (cluster -> workers ==NULL){
+        err("cluster_init():: allocate worker thread on memeory failed \n");
+        return NULL;
+    }
+    
+    for (int i=0; i < num_worker; i ++){
+        
+    }
+
+    return cluster;
+}
 
 
 
 int main(){
-    
+   SERVICE_KEEPALIVE = 1;
+
         
 
 
